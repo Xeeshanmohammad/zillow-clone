67208af4da507834e854b58c35666cedd37fbd79
ZeeshanMeitY <zeeshanmohammad.se@manuu.edu.in>
2025-07-11 12:19:16 +0530

    Final Submit

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(morgan("dev"));
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173","https://zillow-properties.netlify.app"],
     credentials: true,
   })
 );
